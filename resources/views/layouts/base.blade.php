9decd37d35086af07480517d5a66bf6256d09a2b
Tio Muhamad Nur <mtio@jakartamrt.co.id>
2024-10-29 21:17:59 +0700

    update asset

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -11,7 +11,7 @@
     <meta name="description" content="">
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon/favicon.ico') }}">
+    <link rel="icon" type="image/x-icon" href="{{ config('app.asset_url') }}assets/img/favicon/favicon.ico">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -21,27 +21,29 @@
         rel="stylesheet">
 
     <!-- Icons. Uncomment required icon fonts -->
-    <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/boxicons.css') }}">
+    <link rel="stylesheet" href="{{ config('app.asset_url') }}assets/vendor/fonts/boxicons.css">
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/vendor/css/core.css') }}" class="template-customizer-core-css">
-    <link rel="stylesheet" href="{{ asset('assets/vendor/css/theme-default.css') }}"
+    <link rel="stylesheet" href="{{ config('app.asset_url') }}assets/vendor/css/core.css"
+        class="template-customizer-core-css">
+    <link rel="stylesheet" href="{{ config('app.asset_url') }}assets/vendor/css/theme-default.css"
         class="template-customizer-theme-css">
-    <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}">
+    <link rel="stylesheet" href="{{ config('app.asset_url') }}assets/css/demo.css">
 
     <!-- Vendors CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}">
+    <link rel="stylesheet"
+        href="{{ config('app.asset_url') }}assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css">
 
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/apex-charts/apex-charts.css') }}">
+    <link rel="stylesheet" href="{{ config('app.asset_url') }}assets/vendor/libs/apex-charts/apex-charts.css">
 
     <!-- Page CSS -->
 
     <!-- Helpers -->
-    <script src="{{ asset('assets/vendor/js/helpers.js') }}"></script>
+    <script src="{{ config('app.asset_url') }}assets/vendor/js/helpers.js"></script>
 
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="{{ asset('assets/js/config.js') }}"></script>
+    <script src="{{ config('app.asset_url') }}assets/js/config.js"></script>
 </head>
 
 <body>
@@ -69,21 +71,21 @@
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
     {{-- <script src="{{ asset('assets/vendor/libs/jquery/jquery.js') }}"></script> --}}
-    <script src="{{ asset('assets/vendor/libs/popper/popper.js') }}"></script>
-    <script src="{{ asset('assets/vendor/js/bootstrap.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+    <script src="{{ config('app.asset_url') }}assets/vendor/libs/popper/popper.js"></script>
+    <script src="{{ config('app.asset_url') }}assets/vendor/js/bootstrap.js"></script>
+    <script src="{{ config('app.asset_url') }}assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
 
-    <script src="{{ asset('assets/vendor/js/menu.js') }}"></script>
+    <script src="{{ config('app.asset_url') }}assets/vendor/js/menu.js"></script>
     <!-- endbuild -->
 
     <!-- Vendors JS -->
-    <script src="{{ asset('assets/vendor/libs/apex-charts/apexcharts.js') }}"></script>
+    <script src="{{ config('app.asset_url') }}assets/vendor/libs/apex-charts/apexcharts.js"></script>
 
     <!-- Main JS -->
-    <script src="{{ asset('assets/js/main.js') }}"></script>
+    <script src="{{ config('app.asset_url') }}assets/js/main.js"></script>
 
     <!-- Page JS -->
-    <script src="{{ asset('assets/js/dashboards-analytics.js') }}"></script>
+    <script src="{{ config('app.asset_url') }}assets/js/dashboards-analytics.js"></script>
 
     <!-- Place this tag in your head or just before your close body tag. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
